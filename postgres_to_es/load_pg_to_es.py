3ac81b7681b983daaf6f706d1fb28dc41410e3f7
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-12-30 14:07:57 +0300

    рефакт

--- postgres_to_es/load_pg_to_es.py
+++ postgres_to_es/load_pg_to_es.py
@@ -1,9 +1,5 @@
-from pathlib import Path
-
 from common.actions import ElasticIndexCreator, ETLManager
 
-
-BASEDIR = Path(__file__).resolve(strict=True).parent.parent
 
 if __name__ == "__main__":
     create_index = ElasticIndexCreator()
